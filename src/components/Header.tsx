bc487a56f47d3315b423dbc14bcb89639ebef379
giovanna <gigioliv@icloud.com>
2026-01-19 22:24:08 -0300

    fix: adjust logo height in Header component for improved layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,7 +105,7 @@ const Header: React.FC = () => {
                   <img
                     src="/logo_fundo_claro.svg"
                     alt="Logo da loja"
-                    className="h-18 w-auto object-contain"
+                    className="h-11 w-auto object-contain"
                   />
                 </Link>
               </div>
